a4d5a5d4e53ea1bd36f4c83524910a013d1f0d13
rafael-almeida <13210596+rafael-almeida@users.noreply.github.com>
2025-12-27 20:07:23 -0500

    fix layout height

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <main class="flex h-screen flex-1 flex-col bg-gray-100 px-6 py-8">
+  <main class="flex min-h-screen flex-1 flex-col bg-gray-100 px-6 py-8">
     <RouterView />
   </main>
 </template>
